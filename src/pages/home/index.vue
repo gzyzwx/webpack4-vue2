4e992bf3b11abc2e03bd08d34d8b458406fd3d63
zhangweixin <357448012@qq.com>
2022-02-18 14:53:30 +0800

    配置私有npm 仓库地址

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -10,6 +10,10 @@ import { mapGetters, mapState, mapActions } from "vuex"
 import {ccc} from '@/utils/index'
 console.log(ccc('dddd'))
 import aaa from '@/assets/images/demo.jpg'
+// import {dateFormat, copy} from 'hx-utils' // 只有配置npmrc 才能下载
+// console.log(dateFormat, 'dateFormat')
+// console.log(dateFormat('YY-DD', new Date))
+
 export default {
   data() {
     return {
@@ -22,6 +26,14 @@ export default {
   },
   methods: {
     ...mapActions(['setText']),
+    // copyHandler() {
+    //   复制功能，必须有人为事件，否则chrome 不会自动复制 例如播放视频也是无法自动播放
+    //   copy('222').then(res => {
+    //     console.log('success')
+    //   }).catch(err => {
+    //     console.log(err, 'err')
+    //   })
+    // }
   }
 }
 </script>
